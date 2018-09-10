da1473b21dad0ebdf626b8567bcc863995da3bec
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-10 23:53:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,15 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
