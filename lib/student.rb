035111a7f4acaec12e4ecaef29dc8643adb4faf8
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-10 23:57:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,19 +62,19 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      if row[0] < x
-        self.new_from_db(row)
-      end
-    end
-  end
+  # def self.first_X_students_in_grade_10(x)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM students
+  #     WHERE grade = 10
+  #   SQL
+  # 
+  #   DB[:conn].execute(sql).map do |row|
+  #     if row[0] < x
+  #       self.new_from_db(row)
+  #     end
+  #   end
+  # end
 
   def save
     sql = <<-SQL
